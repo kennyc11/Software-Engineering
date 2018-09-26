0965df73cfb3c29b8fd42b7e4d054aba46d43f89
Conor Kenny <kennyc11@tcd.ie>
2018-09-26 16:10:25 +0100

    Commit

--- CS3012 Task 1/src/LCA.java
+++ CS3012 Task 1/src/LCA.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
  
-
+class Node {
+    int data;
+    Node left, right;
+ 
+    Node(int value) {
+        data = value;
+        left = right = null;
+    }
+}
+ 
  
 public class LCA 
 {
