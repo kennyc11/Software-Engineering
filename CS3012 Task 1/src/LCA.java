a72bf2327d28f744267a8a5a6e5ea25c32377583
Conor Kenny <kennyc11@tcd.ie>
2018-09-29 19:48:05 +0100

    Removing line from previous commit and committing new method

--- CS3012 Task 1/src/LCA.java
+++ CS3012 Task 1/src/LCA.java
@@ -14,7 +14,21 @@ class Node {
  
 public class LCA 
 {
- 
-	hello world;
+
 	
+}
+
+private int findLCAInternal(Node root, int n1, int n2) {
+	 
+    if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {
+        return -1;
+    }
+
+    int i;
+    for (i = 0; i < path1.size() && i < path2.size(); i++) {
+        if (!path1.get(i).equals(path2.get(i)))
+            break;
+    }
+
+    return path1.get(i-1);
 }
