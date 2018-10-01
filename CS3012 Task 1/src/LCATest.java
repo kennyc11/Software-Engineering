b4364f4f4b1346d09460c43db7afdd4b0da423d8
Conor Kenny <kennyc11@tcd.ie>
2018-10-01 15:38:24 +0100

    Test added for the existence of one node

--- CS3012 Task 1/src/LCATest.java
+++ CS3012 Task 1/src/LCATest.java
@@ -4,12 +4,23 @@ import org.junit.Test;
 
 public class LCATest {
 	
-	
+	//Test empty tree
 	@Test
 	public void testForEmptyTree() {
 		LCA tree = new LCA();
 		assertEquals(-1,tree.findLCA(0,0));
 	}
 	
+	//Test for existence of one Node
+	@Test
+	public void testForOneNode() {
+		LCA oneNode = new LCA();
+		oneNode.root = new Node(1);
+		assertEquals("The lowest common ancestor of a tree with one node:",-1,oneNode.findLCA(1,0));
+		
+		LCA node = new LCA();
+		node.root = new Node(12);
+		assertEquals(-1,node.findLCA(12, 0));
+	}
 	
 }
