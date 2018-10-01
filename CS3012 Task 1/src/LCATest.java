dcdd300cc632394a5080ab60403bf30c3479dbce
Conor Kenny <kennyc11@tcd.ie>
2018-10-01 22:37:00 +0100

    Running test for when you have same node

--- CS3012 Task 1/src/LCATest.java
+++ CS3012 Task 1/src/LCATest.java
@@ -3,42 +3,42 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LCATest {
-	
+
 	//Test empty tree
 	@Test
 	public void testForEmptyTree() {
 		LCA tree = new LCA();
 		assertEquals(-1,tree.findLCA(0,0));
 	}
-	
+
 	//Test for existence of one Node
 	@Test
 	public void testForOneNode() {
 		LCA oneNode = new LCA();
 		oneNode.root = new Node(1);
 		assertEquals("The lowest common ancestor of a tree with one node:",-1,oneNode.findLCA(1,0));
-		
+
 		LCA node = new LCA();
 		node.root = new Node(12);
 		assertEquals(-1,node.findLCA(12, 0));
 	}
-	
-	
+
+
 	//Test for the existence of two nodes
 	@Test
 	public void testForTwoNodes() {
-		
+
 		LCA twoNodes = new LCA();
 		twoNodes.root = new Node(2);
 		twoNodes.root.right = new Node(3);
 		assertEquals("The lowest common ancestor of a tree with two nodes",2,twoNodes.findLCA(2, 3));
-		
+
 	}
-	
+
 	//Test for tree size 7
 	@Test
 	public void treeSizeSeven() {
-		
+
 		LCA treeSizeSeven = new LCA();
 		treeSizeSeven.root = new Node(1);
 		treeSizeSeven.root.right = new Node(2);
@@ -47,16 +47,16 @@ public class LCATest {
 		treeSizeSeven.root.right.left = new Node(5);
 		treeSizeSeven.root.left.right = new Node(6);
 		treeSizeSeven.root.left.left = new Node(7);
-		
+
 		assertEquals("LCA (1,2)",1,treeSizeSeven.findLCA(1,2));
 		assertEquals("LCA (4,6)",1,treeSizeSeven.findLCA(4,6));
-		
+
 	}
-	
+
 	//Test for non existing nodes
 	@Test
 	public void nonExistentNode() {
-		
+
 		LCA nonExistentNode = new LCA();
 		nonExistentNode.root = new Node(5);
 		nonExistentNode.root.left = new Node(8);
@@ -64,10 +64,25 @@ public class LCATest {
 		nonExistentNode.root.right = new Node(12);
 		nonExistentNode.root.right.right = new Node(13);
 		nonExistentNode.root.right.left = new Node(17);
-		
+
 		assertEquals("LCA(12,21)",-1,nonExistentNode.findLCA(12, 21));
 		assertEquals("LCA(32,17)",-1,nonExistentNode.findLCA(32, 17));
+
+
+	}
+
+	//Test for same nodes
+	@Test
+	public void sameNode() {
+
+		LCA sameNode = new LCA();
+		sameNode.root = new Node(5);
+		sameNode.root.right = new Node(5);
+		sameNode.root.right.right = new Node(5);
+		sameNode.root.left = new Node(5);
+		sameNode.root.left.left = new Node(5);
 		
-		
+		assertEquals("LCA(5,5)",5,sameNode.findLCA(5, 5));
+
 	}
 }
