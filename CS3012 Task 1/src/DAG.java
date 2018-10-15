c006f7f68bec7b43651d80997823b8772369725a
Conor Kenny <kennyc11@tcd.ie>
2018-10-15 18:04:31 +0100

    Declaration of variables

--- CS3012 Task 1/src/DAG.java
+++ CS3012 Task 1/src/DAG.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class DAG {
+	private int V;           // number of vertices in this digraph
+	private int E;                 // number of edges in this digraph
+	private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+	private int[] indegree;        // indegree[v] = indegree of vertex v
+	private boolean marked[];		//Boolean List to track visited vertices
+	private boolean hasCycle;		//True if cycle in graph
+    private boolean stack[];		//Order that vertices were visited
 
 }
