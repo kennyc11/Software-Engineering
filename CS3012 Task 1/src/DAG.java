bb89ac9ef176974385c0a968ee27f8a44a54c2d7
Conor Kenny <kennyc11@tcd.ie>
2018-10-15 18:06:33 +0100

    DAG method

--- CS3012 Task 1/src/DAG.java
+++ CS3012 Task 1/src/DAG.java
@@ -10,6 +10,20 @@ public class DAG {
 	private int[] indegree;        // indegree[v] = indegree of vertex v
 	private boolean marked[];		//Boolean List to track visited vertices
 	private boolean hasCycle;		//True if cycle in graph
-    private boolean stack[];		//Order that vertices were visited
+	private boolean stack[];		//Order that vertices were visited
 
+
+	public DAG(int V)
+	{
+		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+		this.V = V;
+		this.E = 0;
+		indegree = new int[V];
+		marked = new boolean[V];
+		stack = new boolean[V];
+		adj = (ArrayList<Integer>[]) new ArrayList[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new ArrayList<Integer>();
+		}              
+	}
 }
