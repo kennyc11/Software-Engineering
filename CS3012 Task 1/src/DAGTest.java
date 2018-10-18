598b769b480480d9cfe5b31fe200cecdabb82d93
kennyc11 <43179667+kennyc11@users.noreply.github.com>
2018-10-18 11:55:38 +0100

    Test for Directed Graph

--- CS3012 Task 1/src/DAGTest.java
+++ CS3012 Task 1/src/DAGTest.java
@@ -4,9 +4,23 @@ import org.junit.Test;
 
 public class DAGTest {
 
+	//Following tests check Directed Graph class works correctly
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testForDirectedGraph(){
+		DAG test = new DAG(10);
+		test.addEdge(1, 2);
+		test.addEdge(1, 3);
+		test.addEdge(3, 4);
+		test.addEdge(4, 5);
+		test.addEdge(4, 6);
 
+		assertEquals("", 1, test.indegree(5));
+		assertEquals("", 2, test.outdegree(6));
+		assertEquals("Number of edges should be 5", 5, test.E());
+		assertEquals("Number of vertices should be 10", 10, test.V());
+		String ans = "[5, 6]";
+		assertEquals("",ans, test.adj(4).toString());
+
+	}
+	
 }
