24c5a39efcfe329050e5d664ceff1048cdfecd93
kennyc11 <43179667+kennyc11@users.noreply.github.com>
2018-10-18 12:13:03 +0100

    addition of new tests and correction of errors

--- CS3012 Task 1/src/DAGTest.java
+++ CS3012 Task 1/src/DAGTest.java
@@ -15,7 +15,7 @@ public class DAGTest {
 		test.addEdge(4, 6);
 
 		assertEquals("", 1, test.indegree(5));
-		assertEquals("", 2, test.outdegree(6));
+		assertEquals("", 2, test.outdegree(4));
 		assertEquals("Number of edges should be 5", 5, test.E());
 		assertEquals("Number of vertices should be 10", 10, test.V());
 		String ans = "[5, 6]";
@@ -23,4 +23,29 @@ public class DAGTest {
 
 	}
 	
+	@Test
+	public void testAddEdge(){
+		DAG test4 = new DAG(5);
+		//This edge should add
+		test4.addEdge(0, 1);
+
+		//This should print message to console and have no effect
+		test4.addEdge(-2, -5);
+
+		//This should not either as 9>5 
+		test4.addEdge(3, 9);
+		assertEquals("Number of edges should be 1", 1, test4.E());
+	}
+
+	@Test
+	public void testinDegree(){
+		DAG testInDegree = new DAG(5);
+		testInDegree.addEdge(0, 1);
+		testInDegree.addEdge(0, 2);
+		testInDegree.addEdge(1, 2);
+
+		assertEquals("", -1, testInDegree.indegree(-2));
+		assertEquals("", 1, testInDegree.indegree(1));
+		assertEquals("", 2, testInDegree.indegree(2));
+	}
 }
